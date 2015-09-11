d74fa072d5d622a461905e4bdd1414fc3e5954e7
From Zero <jorge@fromzero.io>
2015-09-11 21:34:59 +0000

    modified dbinit

--- dbinit.py
+++ dbinit.py
@@ -6,14 +6,14 @@ from flask_blog import app
 import sqlalchemy
 
 try:
-    db_uri = 'mysql://%s:%s@mysql:3306/' % (app.config['DB_USERNAME'], app.config['DB_PASSWORD'])
+    db_uri = 'mysql+pymysql://%s:%s@%s/' % (app.config['DB_USERNAME'], app.config['DB_PASSWORD'], app.config['DB_HOST'])
     engine = sqlalchemy.create_engine(db_uri)
     conn = engine.connect()
     conn.execute("commit")
     conn.execute("create database "  + app.config['BLOG_DATABASE_NAME'])
     conn.close()
 except:
-    print "Database exists"
+    print("Database exists")
 
 # Create tables for all models
 from flask_blog import db
