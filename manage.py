efdfe2fb41d1f00ff15ab99f3afc282712d3bc77
From Zero <jorge@fromzero.io>
2015-09-11 20:18:35 +0000

    added new c9 host and port

--- manage.py
+++ manage.py
@@ -11,8 +11,8 @@ manager = Manager(app)
 manager.add_command("runserver", Server(
     use_debugger = True,
     use_reloader = True,
-    host = '0.0.0.0',
-    port = "5000")
+    host = os.getenv('IP', '0.0.0.0'),
+    port = int(os.getenv('PORT', 5000)))
 )
 
 if __name__ == "__main__":
