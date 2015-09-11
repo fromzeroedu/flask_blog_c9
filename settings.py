d3efb198a2829acac59d321707d7ef3f132df5cc
From Zero <jorge@fromzero.io>
2015-09-11 21:24:42 +0000

    taking out db creds from this step

--- settings.py
+++ settings.py
@@ -2,9 +2,3 @@ import os
 
 SECRET_KEY = 'you-will-never-guess'
 DEBUG=True
-DB_USER = 'fromzeroedu'
-DB_PASSWORD = '' # not required for cloud9
-DB_DATABASE = 'blog'
-DB_HOST = os.getenv('IP', '0.0.0.0')
-DB_URI = "mysql+pymysql://%s:%s@%s/%s" % (DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE)
-SQLALCHEMY_DATABASE_URI = DB_URI
