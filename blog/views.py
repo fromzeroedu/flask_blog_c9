904eb1cb06303891ef9ef594d72f1038e11ad309
From Zero <jorge@fromzero.io>
2015-09-14 21:24:10 +0000

    finished step-14

--- blog/views.py
+++ blog/views.py
@@ -16,8 +16,7 @@ POSTS_PER_PAGE = 5
 @app.route('/index/<int:page>')
 def index(page=1):
     blog = Blog.query.first()
-    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page, POSTS_PER_PAGE, False)
-    return render_template('blog/index.html', blog=blog, posts=posts)
+    posts = Post.query.filter_by(live=True).order_by(Post.publish_date.desc()).paginate(page, POSTS_PER_PAGE, False)
 
 @app.route('/admin')
 @app.route('/admin/<int:page>')
@@ -88,9 +87,45 @@ def post():
         db.session.add(post)
         db.session.commit()
         return redirect(url_for('article', slug=slug))
-    return render_template('blog/post.html', form=form)
+    return render_template('blog/post.html', form=form, action="new")
     
 @app.route('/article/<slug>')
 def article(slug):
     post = Post.query.filter_by(slug=slug).first_or_404()
     return render_template('blog/article.html', post=post)
+    
+@app.route('/edit/<int:post_id>', methods=('GET', 'POST'))
+@author_required
+def edit(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    form = PostForm(obj=post)
+    if form.validate_on_submit():
+        original_image = post.image
+        form.populate_obj(post)
+        if form.image.has_file():
+            image = request.files.get('image')
+            try:
+                filename = uploaded_images.save(image)
+            except:
+                flash("The image was not uploaded")
+            if filename:
+                post.image = filename
+        else:
+            post.image = original_image
+        if form.new_category.data:
+            new_category = Category(form.new_category.data)
+            db.session.add(new_category)
+            db.session.flush()
+            post.category = new_category
+        db.session.commit()
+        return redirect(url_for('article', slug=post.slug))
+    return render_template('blog/post.html', form=form, post=post, action="edit")
+
+@app.route('/delete/<int:post_id>')
+@author_required
+def delete(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    post.live = False
+    db.session.commit()
+    flash("Article deleted")
+    return redirect('/admin')
