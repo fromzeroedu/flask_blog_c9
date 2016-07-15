f0c7bdb257c6af40d6809078488651cce200cd0e
From Zero <jorge@fromzero.io>
2016-07-15 15:24:43 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.migrate import Migrate
 from flaskext.markdown import Markdown
-from flask_uploads.uploads import UploadSet, configure_uploads, IMAGES
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__)
 app.config.from_object('settings')
